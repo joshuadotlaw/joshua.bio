cf38a62cd837561494738063163602f8cb5157da
Joshua F. Gonzales <j@joshua.bio>
2022-07-24 01:22:50 -0700

    Simplify array construction

--- scripts/uid.js
+++ scripts/uid.js
@@ -1,11 +1,7 @@
 module.exports = function script(len) {
   let id = "";
-  let ascii_range = [48,57];
-  let dict = [];
-  for (let i = ascii_range[0]; i <= ascii_range[1]; i += 1) {
-    dict.push(String.fromCharCode(i));
-  }
-  for (let j = 0; j < len; j += 1) {
+  let dict = "0123456789".split("");
+  for (let i = 0; i < len; i += 1) {
     let random_index;
     random_index = parseInt((Math.random() * dict.length).toFixed(0),10) % dict.length;
     id += dict[random_index];
