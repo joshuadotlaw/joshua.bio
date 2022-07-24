29962d97120351f092f04ca2afcfa2cc5d58c144
Joshua F. Gonzales <j@joshua.bio>
2022-07-24 01:36:46 -0700

    Remove unnecessary intermediary variable

--- scripts/uid.js
+++ scripts/uid.js
@@ -2,9 +2,7 @@ module.exports = function script(len) {
   let id = "";
   let dict = "0123456789".split("");
   for (let i = 0; i < len; i += 1) {
-    let random_index;
-    random_index = parseInt((Math.random() * dict.length).toFixed(0),10) % dict.length;
-    id += dict[random_index];
+    id += dict[parseInt((Math.random() * dict.length).toFixed(0),10) % dict.length];
   }
   return id;
 }
