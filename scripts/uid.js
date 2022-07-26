51674fe6c7f9dfe046f14609f58645912b9d1e3b
Joshua F. Gonzales <j@joshua.bio>
2022-07-25 19:46:58 -0700

    Prevent unwanted substrings in UID

--- scripts/uid.js
+++ scripts/uid.js
@@ -1,8 +1,13 @@
 module.exports = function script(len) {
-  let id = "";
-  let dict = "013456789cfghjmkqrsvwxyz".split("");
+  var id = "";
+  var dict = "013456789cfghjmkqrsvwxyz".split("");
+  var disallowed = ["69","ss","666"];
   for (let i = 0; i < len; i += 1) {
     id += dict[parseInt((Math.random() * dict.length).toFixed(0),10) % dict.length];
+    if (disallowed.some(v => id.includes(v))) {
+      id = "";
+      i = 0;
+    }
   }
   return id;
 }
