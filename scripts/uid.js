04e517969a2dd94eb2901465e7d8502c3c0dd3f1
Joshua F. Gonzales <j@joshua.bio>
2022-07-25 19:55:08 -0700

    More unwanted substrings

--- scripts/uid.js
+++ scripts/uid.js
@@ -1,7 +1,7 @@
 module.exports = function script(len) {
   let id = "";
   let dict = "013456789cfghjmkqrsvwxyz".split("");
-  let disallowed = ["69","ss","666"];
+  let disallowed = ["69","ss","666","s3x","53x","6006s","60065"];
   for (let i = 0; i < len; i += 1) {
     id += dict[parseInt((Math.random() * dict.length).toFixed(0),10) % dict.length];
     if (disallowed.some(v => id.includes(v))) {
