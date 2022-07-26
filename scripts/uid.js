5d74e1dce0469035f47ff0dc8bc0b2661ebfa831
Joshua F. Gonzales <j@joshua.bio>
2022-07-25 22:29:46 -0700

    Break an overly long line

--- scripts/uid.js
+++ scripts/uid.js
@@ -8,7 +8,8 @@ module.exports = function generate_uid(len) {
     "80085","xxx"
   ];
   for (let i = 0; i < len; i += 1) {
-    uid += pool[parseInt((Math.random() * pool.length).toFixed(0),10) % pool.length];
+    uid += pool[parseInt((Math.random() * pool.length)
+      .toFixed(0),10) % pool.length];
     if (unwanted.some(v => uid.includes(v))) {
       uid = "";
       i = 0;
